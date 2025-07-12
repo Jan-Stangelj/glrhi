9738bc2eb855023ea6aa5e82a0c753bfa0c8b1e9
Jan <janstangelj0@protonmail.com>
2025-07-12 11:36:07 +0200

    Created ssbo.hpp

---
+++ include/glrhi/core/ssbo.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <glad/glad.h>
+
+namespace glrhi {
+    class ssbo {
+    public:
+
+        ssbo(GLsizeiptr size, const void* data = nullptr);
+        ~ssbo();
+
+        void sendData(GLintptr offset, GLsizeiptr size, const void* data) const;
+
+        void addBindingPoint(GLuint bindingPoint) const;
+        void resetBindingPoint(GLuint bindingPoint) const;
+
+        GLuint getID() const { return m_ID; }
+
+    private:
+
+        GLuint m_ID = 0;
+
+    };
+}
