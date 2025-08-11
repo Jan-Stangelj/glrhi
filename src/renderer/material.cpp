4892e8214c866115ec9ab07166db95db4b9d04b7
Jan <107766070+Jan-Stangelj@users.noreply.github.com>
2025-08-11 14:49:13 +0200

    Commented material.cpp

--- src/renderer/material.cpp
+++ src/renderer/material.cpp
@@ -1,5 +1,3 @@
-#include "glrhi/core/ebo.hpp"
-#include <cmath>
 #include <filesystem>
 #include <glrhi/renderer/material.hpp>
 
@@ -11,22 +9,27 @@ namespace glrhi {
     }
 
     void material::bind(glrhi::shader& shader) const {
+        // Bind the UBO
         m_matBuffer.addBindingPoint(1);
 
+        // Set the fallback values, if textures are not present
         m_matBuffer.sendData(0, sizeof(GLfloat) * 4, &albedo);
         m_matBuffer.sendData(sizeof(GLfloat) * 4, sizeof(GLfloat) * 4, &arm);
         m_matBuffer.sendData(sizeof(GLfloat) * 8, sizeof(GLfloat) * 4, &emission);
 
+        // Set the "has" values
         m_matBuffer.sendData(sizeof(GLfloat) * 12, sizeof(GLfloat), &m_hasAlbedoTex);
         m_matBuffer.sendData(sizeof(GLfloat) * 13, sizeof(GLfloat), &m_hasARMtex);
         m_matBuffer.sendData(sizeof(GLfloat) * 14, sizeof(GLfloat), &m_hasNormalTex);
         m_matBuffer.sendData(sizeof(GLfloat) * 15, sizeof(GLfloat), &m_hasEmissionTex);
 
+        // Bind textures to texture units
         m_albedoTex.bind(0);
         m_armTex.bind(1);
         m_normalTex.bind(2);
         m_emissionTex.bind(3);
 
+        // Set sampler2D uniforms (sampler2D cannot be in UBO's :( )
         shader.setInt("u_albedo", 0);
         shader.setInt("u_arm", 1);
         shader.setInt("u_normal", 2);
