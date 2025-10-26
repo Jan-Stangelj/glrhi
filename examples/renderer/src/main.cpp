701b8a4398cab300b19b8ab45d059eb9f905d181
Jan <107766070+Jan-Stangelj@users.noreply.github.com>
2025-10-26 20:06:54 +0100

    GI with multiple cone traces

--- examples/renderer/src/main.cpp
+++ examples/renderer/src/main.cpp
@@ -12,7 +12,7 @@ int main()
     unsigned int sponza = scene.addModel("../examples/renderer/sponza/Sponza.gltf");
     scene.getModel(sponza).size = glm::vec3(0.01f);
 
-    scene.sunDir = glm::vec4(0.25f, 1.0f, 0.25f, 1.0f);
+    scene.sunDir = glm::vec4(0.35f, 1.0f, 0.35f, 1.0f);
     scene.sunColor = glm::vec4(1.0f);
     scene.sunStrenght = 25.0f;
     scene.sunAperture = 3.0f;
